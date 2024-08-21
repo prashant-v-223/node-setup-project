47533dcd62a1ce84eb75ee815861c13b4bfda43b
prashant-v-223 <prashantvadhvana@gmail.com>
2024-08-21 13:03:03 +0530

    ee

--- models/Servi.js
+++ models/Servi.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
-const WorksSchema = new mongoose.Schema({
+const ServiSchema = new mongoose.Schema({
     pImg: { type: String, required: true }, // Main image
     ps1img: { type: String, required: false }, // Secondary image
     title: { type: String, required: true },
     filterName: { type: String, required: false },
     description: { type: String, required: true },
-    description1: { type: String, required: false },
     type: { type: String, enum: ['work', 'blog', 'client'], required: true }  // New field to differentiate
 },
     {
         timestamps: true,
     });
 
-module.exports = mongoose.model('Works', WorksSchema);
+module.exports = mongoose.model('Servi', ServiSchema);
